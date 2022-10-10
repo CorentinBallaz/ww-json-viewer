c2d377cdfde42f738f5709b43f1901066682cd13
CorentinBallaz <corentin.ballaz@steady-sun.com>
2022-10-10 10:10:21 +0200

    first commit

---
+++ src/wwElement.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app">
+    <json-viewer :value="jsonData"></json-viewer>
+  </div>
+</template>
+
+<script>
+import { JsonViewer } from "vue-json-viewer";
+import 'vue-json-viewer/style.css';
+
+export default {
+  components: { JsonViewer },
+  props: {
+    content: { type: Object, required: true },
+  },
+  computed: {
+    jsonData() {
+      return {
+          total: 25,
+          limit: 10,
+          skip: 0,
+          links: {
+            previous: undefined,
+            next: function () {},
+          },
+          data: this.content.data,
+        };     
+      },
+    },
+  }
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ ww-config.js
@@ -0,0 +1,18 @@
+export default {
+  editor: {
+    label: {
+      en: "Json Viewer",
+    },
+  },
+  properties: {
+    data: {
+      label: {
+        en: "Data",
+      },
+      type: "Info",
+      defaultValue: [],
+      bindable: true,
+      responsive: true,
+    },
+  },
+};
